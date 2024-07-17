559ace16198f62ec9b834d92f5589f085f2dab2a
navneeth-dev <153168398+navneeth-dev@users.noreply.github.com>
2024-07-17 19:00:39 +0530

    Update greedySnapSack.c

--- LAB12/greedySnapSack.c
+++ LAB12/greedySnapSack.c
@@ -1,74 +1,75 @@
 #include<stdio.h>
-void knapsack(int n, float weight[], float profit[], float capacity) 
-{
-   float x[20], tp = 0;
-   int i, j, u;
-   u = capacity;
 
-   for (i = 0; i < n; i++)
-      x[i] = 0.0;
+void knapsack(int n, float weight[], float profit[], float capacity) {
+    float x[20], totalProfit = 0;
+    int i, j, remainingCapacity;
+    remainingCapacity = capacity;
 
-   for (i = 0; i < n; i++) {
-      if (weight[i] > u)
-         break;
-      else {
-         x[i] = 1.0;
-         tp = tp + profit[i];
-         u = u - weight[i];
-      }
-   }
+    for (i = 0; i < n; i++)
+        x[i] = 0.0;
 
-   if (i < n)
-      x[i] = u / weight[i];
+    for (i = 0; i < n; i++) {
+        if (weight[i] > remainingCapacity)
+            break;
+        else {
+            x[i] = 1.0;
+            totalProfit += profit[i];
+            remainingCapacity -= weight[i];
+        }
+    }
 
-   tp = tp + (x[i] * profit[i]);
+    if (i < n)
+        x[i] = remainingCapacity / weight[i];
 
-   printf("\nThe result vector is:- ");
-   for (i = 0; i < n; i++)
-      printf("%f\t", x[i]);
+    totalProfit += x[i] * profit[i];
 
-   printf("\nMaximum profit is:- %f", tp);
+    printf("\nThe result vector is: ");
+    for (i = 0; i < n; i++)
+        printf("%f\t", x[i]);
 
+    printf("\nMaximum profit is: %f", totalProfit);
 }
 
 int main() {
-   float weight[20], profit[20], capacity;
-   int num, i, j;
-   float ratio[20], temp;
+    float weight[20], profit[20], capacity;
+    int num, i, j;
+    float ratio[20], temp;
 
-   printf("\nEnter the no. of objects:- ");
-   scanf("%d", &num);
+    printf("\nEnter the number of objects: ");
+    scanf("%d", &num);
 
-   printf("\nEnter the wts and profits of each object:- ");
-   for (i = 0; i < num; i++) {
-      scanf("%f %f", &weight[i], &profit[i]);
-   }
+    printf("\nEnter the weights and profits of each object: ");
+    for (i = 0; i < num; i++) {
+        scanf("%f %f", &weight[i], &profit[i]);
+    }
 
-   printf("\nEnter the capacity of knapsack:- ");
-   scanf("%f", &capacity);
+    printf("\nEnter the capacity of the knapsack: ");
+    scanf("%f", &capacity);
 
-   for (i = 0; i < num; i++) {
-      ratio[i] = profit[i] / weight[i];
-   }
+    for (i = 0; i < num; i++) {
+        ratio[i] = profit[i] / weight[i];
+    }
 
-   for (i = 0; i < num; i++) {
-      for (j = i + 1; j < num; j++) {
-         if (ratio[i] < ratio[j]) {
-            temp = ratio[j];
-            ratio[j] = ratio[i];
-            ratio[i] = temp;
+    // Sort objects based on profit/weight ratio in descending order
+    for (i = 0; i < num; i++) {
+        for (j = i + 1; j < num; j++) {
+            if (ratio[i] < ratio[j]) {
+                temp = ratio[j];
+                ratio[j] = ratio[i];
+                ratio[i] = temp;
 
-            temp = weight[j];
-            weight[j] = weight[i];
-            weight[i] = temp;
+                temp = weight[j];
+                weight[j] = weight[i];
+                weight[i] = temp;
 
-            temp = profit[j];
-            profit[j] = profit[i];
-            profit[i] = temp;
-         }
-      }
-   }
+                temp = profit[j];
+                profit[j] = profit[i];
+                profit[i] = temp;
+            }
+        }
+    }
 
-   knapsack(num, weight, profit, capacity);
-   return(0);
+    knapsack(num, weight, profit, capacity);
+    return 0;
 }
+
